7773373a83234caa3e2b0f3be6f356623f35fdd3
cpk0709 <cpk0709@gmail.com>
2022-06-15 19:54:44 +0900

    파이어베이스 추가 및 테스트 스크립트 삭제

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBggvwVJdbLnSdM1U_XpZf_ubalWMotd6I",
+    authDomain: "nwitter-f675e.firebaseapp.com",
+    projectId: "nwitter-f675e",
+    storageBucket: "nwitter-f675e.appspot.com",
+    messagingSenderId: "514889100722",
+    appId: "1:514889100722:web:bb39d2ae9f04f6f20748ab"
+};
+
+  // Initialize Firebase
+export default initializeApp(firebaseConfig);
