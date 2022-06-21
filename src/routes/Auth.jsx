f13253300ab18a016bc44b9bfbc9dc426976e828
cpk0709 <cpk0709@gmail.com>
2022-06-21 14:07:17 +0900

    로그인 폼 작성

--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,9 +1,17 @@
 const Auth = () => {
-    return (
-        <div>
-            Auth
-        </div>
-    )
-}
+  return (
+    <div>
+      <form>
+        <input type='text' placeholder='Email' required />
+        <input type='password' placeholder='Password' required />
+        <input type='submit' value='Log In' />
+      </form>
+      <div>
+        <button>Contiune with Google</button>
+        <button>Contiune with Github</button>
+      </div>
+    </div>
+  );
+};
 
 export default Auth;
