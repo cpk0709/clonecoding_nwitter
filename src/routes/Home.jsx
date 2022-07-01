53485143bf60d2cda609afc3c662416c34ca4727
cpk0709 <cpk0709@gmail.com>
2022-07-01 23:59:51 +0900

    form태그 submit 추가

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,21 @@
+import { useState } from 'react';
+
 const Home = () => {
+  const [nweet, setNweet] = useState('');
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {};
   return (
     <div>
-      <form>
-        <input type='text' placeholder="What's on your mind?" maxLength={120} />
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type='text'
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
         <input type='submit' value='Nweet' />
       </form>
     </div>
