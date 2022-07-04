aec91296c2b49e323b1e4787235f0e7c1f397b91
cpk0709 <cpk0709@gmail.com>
2022-07-04 22:03:48 +0900

    firestore 컬렉션 도큐먼트 명 일부 변경

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -12,6 +12,7 @@ import { useEffect } from 'react';
 const Home = () => {
   const [nweet, setNweet] = useState('');
   const [nweets, setNweets] = useState([]);
+
   const getNweets = async () => {
     const q = query(collection(dbService, 'nweets'));
     const querySnapshot = await getDocs(q);
@@ -23,13 +24,15 @@ const Home = () => {
       setNweets((prev) => [nweetObj, ...prev]);
     });
   };
+
   useEffect(() => {
     getNweets();
   }, []);
+
   const onSubmit = async (event) => {
     event.preventDefault();
     await addDoc(collection(dbService, 'nweets'), {
-      nweet,
+      text: nweet,
       createdAt: serverTimestamp(),
     });
     setNweet('');
@@ -55,7 +58,11 @@ const Home = () => {
         </form>
       </div>
       {nweets.map((nweet, index) => {
-        return <div key={index}>{nweet.nweet}</div>;
+        return (
+          <div key={index}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        );
       })}
     </>
   );
