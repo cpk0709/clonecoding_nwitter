49296e580a13340cde669b8f83b66f306a31ed3f
cpk0709 <cpk0709@gmail.com>
2022-07-05 11:19:26 +0900

    트윗 삭제 기능 추가

--- src/components/Nweet.jsx
+++ src/components/Nweet.jsx
@@ -1,11 +1,20 @@
+import { dbService } from 'myFirebase';
+import { doc, deleteDoc } from 'firebase/firestore';
+
 const Nweet = ({ nweetObj, isOwner }) => {
-  console.log(isOwner);
+  const onDeleteClick = async () => {
+    const ok = window.confirm('Are you sure you want to delete this nweet?');
+    if (ok) {
+      const NweetTextRef = doc(dbService, 'nweets', nweetObj.id);
+      await deleteDoc(NweetTextRef);
+    }
+  };
   return (
     <div>
       <h4>{nweetObj.text}</h4>
       {isOwner && (
         <>
-          <button>Delete Nweet</button>
+          <button onClick={onDeleteClick}>Delete Nweet</button>
           <button>Edit Nweet</button>
         </>
       )}
