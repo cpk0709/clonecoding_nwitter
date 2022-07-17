c88a8b034fae7c9affec8b50a6b362eafeb7fac1
cpk0709 <cpk0709@gmail.com>
2022-07-18 01:13:32 +0900

    index.js EditProfile.jsx 삭제

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 import Auth from './Auth';
 import Home from './Home';
 import Profile from './Profile';
-import EditProfile from './EditProfile';
 
-export { Auth, Home, Profile, EditProfile };
+export { Auth, Home, Profile };
